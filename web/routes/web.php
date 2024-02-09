d451ab1f3af75a4a15fefe05b0efe53fbe74b924
webitp <webitp.off@gmail.com>
2024-02-09 11:30:24 +0300

    Test

--- web/routes/web.php
+++ web/routes/web.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
+
 Route::group(['middleware' => ['auth', 'admin']], function () {
     Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
